086407e81d636a1eace870333edbe9972d58027b
Arturo Martinez Delgado <arturomtzdel17@gmail.com>
2025-06-29 17:18:11 -0600

    first commit

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def predict():
         return jsonify({'error': 'Error interno en el servidor'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
